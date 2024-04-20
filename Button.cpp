e2f440a1fbe4384ea1437b9a9b5298d77eef4f63
Yash <yash.4.karan@Gmail.com>
2024-04-20 23:33:24 +0100

    Initial commit.

---
+++ Button.cpp
@@ -0,0 +1,56 @@
+#include "Arduino.h"
+#include "Button.h"
+
+Button::Button(byte pin, bool def, unsigned int dt) {
+    _pin = pin;
+    _def = def;
+    _dt = dt;
+    pinMode(pin, INPUT);
+}
+
+Button::Button(byte pin, bool def) {
+    _pin = pin;
+    _def = def;
+    pinMode(pin, INPUT);
+}
+
+Button::Button(byte pin) {
+    _pin = pin;
+    pinMode(pin, INPUT);
+}
+
+bool Button::Status() { // current state of the button pin: 0 is not pressed, 1 is pressed
+    return (digitalRead(_pin) != _def); // of digitalRead == _def, the button is not pressed
+}
+
+bool Button::IsToggled() { // returns true when there is a toggle i.e. a state change, does NOT return the state of the toggle
+    return IsLongPressed(_dt);
+}
+
+bool Button::IsLongPressed(unsigned long lpt) { // need to make either long press or toggle option available?
+    if (digitalRead(_pin) == !_def) { // button pressed
+        if (_bp == 0) {
+            _tbp = millis();
+            _bp = 1;
+            _apt = 1;
+            return false;
+        }
+        else if (!_t && _apt && millis() >= _tbp + lpt) {
+            _t = 1;
+            _apt = 0;
+            //Serial.println(millis());
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else { // button not pressed/released
+        if (!_apt || millis() >= _tbp + _dt) {
+            _bp = 0;
+            _apt = 0;
+            _t = 0;
+            return false;
+        }
+    }
+}
---
+++ Button.h
@@ -0,0 +1,35 @@
+#ifndef MyClass_h
+#define MyClass_h
+
+#include "Arduino.h"
+
+class Button {
+  private:
+    byte _pin;  // pin number of button
+    unsigned int _dt = 10; // debounce time (configurable)
+    unsigned long _tbp = 0; // start time of the button being pressed
+    bool _apt = 0; // awaiting press time. Start waiting for long press, but only while the button is pressed.
+    bool _t = 0; // has the button been toggled already? Prevents repeated toggles when button remains pressed for longer.
+    bool _bp = 0; // button pressed?
+    bool _def = 1; // default position, LOW or HIGH. In this case, the default is HIGH, button press causes pin to go LOW
+
+  public:
+    // constructor
+    Button(byte pin, bool def, unsigned int dt);
+    Button(byte pin, bool def);
+    Button(byte pin);
+
+    // live status of pin
+    bool Status();
+
+    // function that returns true when toggled. If the button is kept pressed, no not continually switch states.
+    bool IsToggled();
+
+    // function that returns true when long-pressed. If the button is kept pressed, no not continually switch states.
+    bool IsLongPressed(unsigned long lpt);
+
+    // implement a 'held down' function with debounce?
+    // bool HeldDown();
+};
+
+#endif
